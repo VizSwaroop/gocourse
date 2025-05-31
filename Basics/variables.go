5b338f4f110a7f908ed0889529b9968941735587
VizSwaroop <208224872+VizSwaroop@users.noreply.github.com>
2025-05-31 09:33:08 +0530

    Hello to naming conventions

---
+++ Basics/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	fmt.Println(middleName)
+	printname()
+}
+
+func printname() {
+	firstName := "Michael"
+	fmt.Println(firstName)
+}
---
+++ naming_conventions.go
@@ -0,0 +1,17 @@
+package main
+
+func main1() {
+	//PascalCase
+	//Eg. CalculateArea,UserInfo,NewHTTPRequest
+	//structs, interfaces, enums
+
+	//snake_case
+	//Eg. user_id,first_name,http_request
+
+	//UPPERCASE
+	//USse case is constants
+
+	// mixedCase
+	// Eg. javaScript,htmlDocument,isValid
+
+}
